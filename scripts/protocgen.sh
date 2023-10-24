72888373fedbb62a9561deee42babf33ce7f14dc
Reece Williams <reecepbcups@gmail.com>
2023-10-24 09:49:38 -0500

    attempt to use SDK's proto, no luck

--- scripts/protocgen.sh
+++ scripts/protocgen.sh
@@ -10,7 +10,7 @@ for dir in $proto_dirs; do
     # this regex checks if a proto file has its go_package set to github.com/strangelove-ventures/poa/...
     # gogo proto files SHOULD ONLY be generated if this is false
     # we don't want gogo proto to run for proto files which are natively built for google.golang.org/protobuf
-    if grep -q "option go_package" "$file" && grep -H -o -c 'option go_package.*github.com/strangelove-ventures/poa/api' "$file" | grep -q ':0$'; then
+    if grep -q "option go_package" "$file" && grep -H -o -c 'option go_package.*github.com/strangelove-ventures/poa/api' "$file" | grep -q ':0$'; then      
       buf generate --template buf.gen.gogo.yaml $file
     fi
   done
