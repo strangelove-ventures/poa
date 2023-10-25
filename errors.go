f016563bb4b326bd8da91f08f457d94f24a203e1
Reece Williams <reecepbcups@gmail.com>
2023-10-25 15:28:36 -0500

    untested commission range limits

--- errors.go
+++ errors.go
@@ -8,5 +8,5 @@ var (
 	ErrStakingActionNotAllowed = sdkerrors.Register(ModuleName, 1, "staking actions are now allowed on this chain")
 	ErrPowerBelowMinimum       = sdkerrors.Register(ModuleName, 2, "power must be above 1_000_000")
 	ErrNotAnAuthority          = sdkerrors.Register(ModuleName, 3, "not an authority")
-	ErrUnsafePower             = sdkerrors.Register(ModuleName, 4, "unsafe: msg.Power is >30%% of total power, set unsafe=true to override")
+	ErrUnsafePower             = sdkerrors.Register(ModuleName, 4, "unsafe: msg.Power is >30%% of total power, set unsafe=true to override")	
 )
