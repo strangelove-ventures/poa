280ddc704cd5dda0dc9358d4cd0b77c1e8775989
Reece Williams <reecepbcups@gmail.com>
2023-11-25 17:19:10 -0600

    add TestRegisterLegacyAminoCodec

--- codec_test.go
+++ codec_test.go
@@ -3,37 +3,41 @@ package poa
 import (
 	"testing"
 
-	"github.com/stretchr/testify/suite"
-
+	"github.com/cosmos/cosmos-sdk/codec"
 	codectypes "github.com/cosmos/cosmos-sdk/codec/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/stretchr/testify/require"
 )
 
-const (
-	prefix = "/strangelove_ventures.poa.v1."
-)
+const prefix = "/strangelove_ventures.poa.v1."
 
-type CodecTestSuite struct {
-	suite.Suite
-}
-
-func TestCodecSuite(t *testing.T) {
-	suite.Run(t, new(CodecTestSuite))
-}
-
-func (suite *CodecTestSuite) TestRegisterInterfaces() {
+func TestCodecRegisterInterfaces(t *testing.T) {
 	registry := codectypes.NewInterfaceRegistry()
 	registry.RegisterInterface(sdk.MsgInterfaceProtoName, (*sdk.Msg)(nil))
 	RegisterInterfaces(registry)
 
 	impls := registry.ListImplementations(sdk.MsgInterfaceProtoName)
 
-	suite.Require().Len(impls, 5)
-	suite.Require().ElementsMatch([]string{
+	require.Len(t, impls, 5)
+	require.ElementsMatch(t, []string{
 		prefix + "MsgSetPower",
 		prefix + "MsgCreateValidator",
 		prefix + "MsgUpdateParams",
 		prefix + "MsgRemoveValidator",
 		prefix + "MsgUpdateStakingParams",
 	}, impls)
+}
+
+func TestRegisterLegacyAminoCodec(t *testing.T) {
+	cdc := codec.NewLegacyAmino()
+	RegisterLegacyAminoCodec(cdc)
+
+	bz, err := cdc.MarshalJSON(MsgSetPower{
+		Sender:           "sender",
+		ValidatorAddress: "validator",
+		Power:            1_234_567,
+		Unsafe:           true,
+	})
+	require.NoError(t, err)
+	require.Equal(t, `{"type":"poa/MsgSetPower","value":{"sender":"sender","validator_address":"validator","power":"1234567","unsafe":true}}`, string(bz))
 }
