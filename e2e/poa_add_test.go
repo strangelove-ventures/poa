64770aeaa612a60c6d47bb6b6d6c18393469efa0
Reece Williams <reecepbcups@gmail.com>
2024-04-05 18:55:03 -0500

    fix(test): poa add val logic

--- e2e/poa_add_test.go
+++ e2e/poa_add_test.go
@@ -1,18 +1,16 @@
 package e2e
 
 import (
-	"context"
 	"fmt"
 	"testing"
-	"time"
 
 	sdkmath "cosmossdk.io/math"
 	stakingtypes "github.com/cosmos/cosmos-sdk/x/staking/types"
+	"github.com/rs/zerolog/log"
 
+	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/strangelove-ventures/interchaintest/v8"
 	"github.com/strangelove-ventures/interchaintest/v8/chain/cosmos"
-	"github.com/strangelove-ventures/interchaintest/v8/ibc"
-	"github.com/strangelove-ventures/interchaintest/v8/testutil"
 	"github.com/strangelove-ventures/poa/e2e/helpers"
 	"github.com/stretchr/testify/require"
 )
@@ -36,9 +34,13 @@ func TestPOAAddValidator(t *testing.T) {
 	cfg.Env = []string{
 		fmt.Sprintf("POA_ADMIN_ADDRESS=%s", "cosmos1hj5fveer5cjtn4wd6wstzugjfdxzl0xpxvjjvr"), // acc0 / admin
 	}
+	cfg.ModifyGenesisAmounts = func() (sdk.Coin, sdk.Coin) {
+		var delegation int64 = 1_000_000000
+		return sdk.NewCoin(cfg.Denom, sdkmath.NewInt(userFunds.Int64())), sdk.NewCoin(cfg.Denom, sdkmath.NewInt(delegation))
+	}
 
 	// setup base chain
-	chains := interchaintest.CreateChainWithConfig(t, 1, 0, "poa", "", POACfg)
+	chains := interchaintest.CreateChainWithConfig(t, 1, 0, "poa", "", cfg)
 	chain := chains[0].(*cosmos.CosmosChain)
 
 	enableBlockDB := false
@@ -59,13 +61,6 @@ func TestPOAAddValidator(t *testing.T) {
 	assertSignatures(t, ctx, chain, 1)
 	assertConsensus(t, ctx, chain, 1)
 
-	// === Test Cases ===
-	testAddValidator(t, ctx, chain, admin, fakeVal)
-	time.Sleep(1_000 * time.Minute)
-
-}
-
-func testAddValidator(t *testing.T, ctx context.Context, chain *cosmos.CosmosChain, admin, fakeVal ibc.Wallet) {
 	// create a new validator and ensure it is added to the consensus set, but assert signatures should still not be
 
 	// pubKeyJSON := `{"@type":"/cosmos.crypto.ed25519.PubKey","key":"pl3Q8OQwtC7G2dSqRqsUrO5VZul7l40I+MKUcejqRsg="}`
@@ -77,23 +72,22 @@ func testAddValidator(t *testing.T, ctx context.Context, chain *cosmos.CosmosCha
 
 	txRes, err := helpers.POACreatePendingValidator(t, ctx, chain, fakeVal, pubKeyJSON, moniker, commissionRate, commissionMaxRate, commissionMaxChangeRate)
 	require.NoError(t, err)
-	fmt.Println("txRes: ", txRes)
+	log.Debug().Msgf("Create pending validator: %v", txRes)
 
 	pending := helpers.GetPOAPending(t, ctx, chain)
 
 	// set power from Gov
 	txRes, err = helpers.POASetPower(t, ctx, chain, admin, pending.Pending[0].OperatorAddress, 1_000_000)
 	require.NoError(t, err)
-	fmt.Println("set power: ", txRes)
-
-	require.NoError(t, testutil.WaitForBlocks(ctx, 5, chain))
+	log.Debug().Msgf("Set pending's power: %v", txRes)
 
 	vals, err := chain.StakingQueryValidators(ctx, stakingtypes.BondStatusBonded)
 	require.NoError(t, err)
-	fmt.Println("Validators: ", vals)
+	log.Debug().Msgf("Validators: %v", vals)
 
 	require.Equal(t, 2, len(vals))
 
 	assertSignatures(t, ctx, chain, 1) // They are not signing, should be 0
 	assertConsensus(t, ctx, chain, 2)  // ensure they were added to CometBFT
+
 }
