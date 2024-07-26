91d97bfca5e9e16f238e34b24ac2f9105d98eb3f
Felix C. Morency <1102868+fmorency@users.noreply.github.com>
2024-07-26 14:18:18 -0400

    fix!: refactor error propagation

--- ante/disable_staking.go
+++ ante/disable_staking.go
@@ -16,49 +16,49 @@ func NewPOADisableStakingDecorator() MsgStakingFilterDecorator {
 }
 
 // AnteHandle performs an AnteHandler check that returns an error if the tx contains a message that is blocked.
-func (msfd MsgStakingFilterDecorator) AnteHandle(ctx sdk.Context, tx sdk.Tx, simulate bool, next sdk.AnteHandler) (newCtx sdk.Context, err error) {
+func (msfd MsgStakingFilterDecorator) AnteHandle(ctx sdk.Context, tx sdk.Tx, simulate bool, next sdk.AnteHandler) (sdk.Context, error) {
 	currHeight := ctx.BlockHeight()
 	if currHeight <= 1 {
 		// allow GenTx to pass
 		return next(ctx, tx, simulate)
 	}
 
-	if msfd.hasInvalidStakingMsg(tx.GetMsgs()) {
-		return ctx, poa.ErrStakingActionNotAllowed
+	err := msfd.hasInvalidStakingMsg(tx.GetMsgs())
+	if err != nil {
+		return ctx, err
 	}
 
 	return next(ctx, tx, simulate)
 }
 
-func (msfd MsgStakingFilterDecorator) hasInvalidStakingMsg(msgs []sdk.Msg) bool {
+func (msfd MsgStakingFilterDecorator) hasInvalidStakingMsg(msgs []sdk.Msg) error {
 	for _, msg := range msgs {
 		// authz nested message check (recursive)
 		if execMsg, ok := msg.(*authz.MsgExec); ok {
 			msgs, err := execMsg.GetMessages()
 			if err != nil {
-				return true
+				return err
 			}
 
-			if msfd.hasInvalidStakingMsg(msgs) {
-				return true
+			err = msfd.hasInvalidStakingMsg(msgs)
+			if err != nil {
+				return err
 			}
 		}
 
 		switch msg.(type) {
 		// POA wrapped messages
-		case *stakingtypes.MsgCreateValidator, *stakingtypes.MsgUpdateParams:
-			return true
-
-		// Blocked entirely when POA is enabled
-		case *stakingtypes.MsgBeginRedelegate,
+		case *stakingtypes.MsgCreateValidator, *stakingtypes.MsgUpdateParams,
+			// Blocked entirely when POA is enabled
+			*stakingtypes.MsgBeginRedelegate,
 			*stakingtypes.MsgCancelUnbondingDelegation,
 			*stakingtypes.MsgDelegate,
 			*stakingtypes.MsgUndelegate:
-			return true
+			return poa.ErrStakingActionNotAllowed
 		}
 
 		// stakingtypes.MsgEditValidator is the only allowed message. We do not need to check for it.
 	}
 
-	return false
+	return nil
 }
